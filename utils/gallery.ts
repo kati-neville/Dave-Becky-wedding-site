9236bc47fcf8b2f5a2699e90523d722f853f6d79
Neville Kati <nevillecodes98@gmail.com>
2022-10-29 20:43:08 +0000

    .

--- utils/gallery.ts
+++ utils/gallery.ts
@@ -7,7 +7,7 @@ import Gal5 from "../public/images/gal5.jpg";
 import Gal6 from "../public/images/gal6.jpg";
 import Gal7 from "../public/images/both-5.jpg";
 import Gal8 from "../public/images/both.jpg";
-import Gal9 from "../public/images/Gal7.jpg";
+import Gal9 from "../public/images/gal7.jpg";
 
 export const Gallery: StaticImageData[] = [
 	Gal1,
